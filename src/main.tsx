db8c894782e7e2a45235e17fdf5b1f53d970f3c7
King-AJr <talk2king.aj@gmail.com>
2026-08-19 10:17:31 +0100

    Initialize session replay and API correlation

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { initBrowserObservability, installObservedFetch } from "@/lib/observability";
+
+initBrowserObservability();
+installObservedFetch();
 
 createRoot(document.getElementById("root")!).render(<App />);
